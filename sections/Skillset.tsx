f966cb8b60e8cb3ee0e8abd87bfd5137f0c89f66
Gradey Cullins <gradeycullins@gmail.com>
2022-12-28 11:23:31 -0700

    update skillset section

--- sections/Skillset.tsx
+++ sections/Skillset.tsx
@@ -6,22 +6,6 @@ const Skillset = () => (
       a preference for working in *nix OSes.
     </p>
     <p>
-      School taught me early on that in order to work in this industry, one must never stop learning.
-      With this principle in mind, I take an active interest in improving skills corolary to programming in order to bolster my technical arsenal.
-    </p>
-    <ul className="sq-li">
-      <li className="mb2">
-       <b>devops</b> - containerization, deployment strategies and CICD pipelines
-      </li>
-      <li className="mb2">
-        <b>testing</b> - unit, regression, integration, e2e testing and performance metrics
-      </li>
-      <li className="mb2">
-        <b>security</b> - functional understanding of cryptographically secured protocols, auth flows, and
-        other server and client-side security considerations
-      </li>
-    </ul>
-    <p>
       Below is a non-exhaustive list of fluent and familiar programming languages that I have used in academic or
       professional settings. 
     </p>
