bc7db570b40c4ba636f3996c7f33c60cdd3bbe93
Gradey Cullins <gradeycullins@gmail.com>
2025-10-21 23:29:29 -0600

    fill in about page

--- app/javascript/pages/AboutPage.tsx
+++ app/javascript/pages/AboutPage.tsx
@@ -1,14 +1,13 @@
 import {Head} from '@inertiajs/react'
 import {BlogPost} from "../types.ts";
 import HookIntro from "../components/HookIntro.tsx";
-import AboutMe from "../components/AboutMe.tsx";
+import "../assets/styles/AboutStyles.css"
 
 type AboutProps = {
   posts: BlogPost[]
 }
 
 export default function AboutPage({}: AboutProps) {
-  console.log('heeere')
   return (
     <>
       <Head title="Gradey Cullins - Personal Website"/>
